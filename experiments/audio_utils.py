437c474f4f8f910090fe27a9dd5033854c4dc053
John Doe <johndoe@example.com>
2024-03-09 11:49:42 +0200

    add nemo notebook

--- experiments/audio_utils.py
+++ experiments/audio_utils.py
@@ -4,32 +4,57 @@ import speech_recognition as sr
 from tqdm import tqdm
 tqdm.pandas()
 import whisper
+import wave
+import librosa
+from scipy.io import wavfile
+import soundfile as sf
 
 
-model = whisper.load_model("medium")
+model = whisper.load_model("base")
 selected_keys = {'ru', 'tr', 'ar'}
 
 def detect_language(audio_file):
-    full_path = os.path.join('data/wav_files_subset', audio_file)
-
     # load audio and pad/trim it to fit 30 seconds
-    audio = whisper.load_audio(full_path)
+    audio = whisper.load_audio(audio_file)
     audio = whisper.pad_or_trim(audio)
 
     # make log-Mel spectrogram and move to the same device as the model
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
     # detect the spoken language
-    _, probs = model.detect_language(mel)
+    dummy, probs = model.detect_language(mel)
 
     selected_values = {key: probs[key] for key in selected_keys}
 
     pred_lang = max(selected_values, key=probs.get)
     return pred_lang
 
+def resample(audio_file, sample_rate, out_path):
+    x, sr = librosa.load(audio_file)
+    sf.write(out_path, x, samplerate=sample_rate)
 
-def predict_language(df):
-    df['pred_lang'] = df['file'].progress_apply(detect_language)
+def preprocess(audio_file):
+    full_path = os.path.join('data/wav_files_subset', audio_file)
+    resample_path = os.path.join('data/resampled', audio_file)
+
+    with wave.open(full_path, "rb") as wav_file:
+        # Get the sample rate
+        sample_rate = wav_file.getframerate()
+        nchannels = wav_file.getnchannels()
+
+    pred_lang = detect_language(full_path)
+    resample(full_path, 16_000, resample_path)
+
+    return sample_rate, nchannels, pred_lang
+
+
+def pre_processing(df):
+    df[
+        'sample_rate',
+        'nchannels',
+        'pred_lang'
+    ] = df['file'].progress_apply(preprocess)
+
     return df
 
 
@@ -50,13 +75,13 @@ def main():
     csv_file = "data/hackathon_train_subset.csv"  # Replace with your CSV file path
     df = pd.read_csv(csv_file)
     df['lang'] = df['language'].map(langdict)
-    # df = df.head(10)
+    df = df.head(10)
 
     # Predict language
-    df_with_pred_lang = predict_language(df)
+    pre_processing_df = pre_processing(df)
 
     # Calculate accuracy
-    accuracy = calculate_accuracy(df_with_pred_lang)
+    accuracy = calculate_accuracy(pre_processing_df)
     print("Accuracy: {:.2f}%".format(accuracy))
 
 
--- nemo.py
+++
@@ -1,3 +0,0 @@
-import nemo.collections.asr as nemo_asr
-speaker_model = nemo_asr.models.EncDecSpeakerLabelModel.from_pretrained(model_name='titanet_small')
-decision = speaker_model.verify_speakers('path/to/one/audio_file','path/to/other/audio_file')
