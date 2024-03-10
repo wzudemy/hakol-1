da9fdf60390a26619e33244915f7b3d7bc4065d6
John Doe <johndoe@example.com>
2024-03-10 21:23:24 +0200

    add score

--- experiments/audio_utils.py
+++ experiments/audio_utils.py
@@ -9,6 +9,8 @@ import librosa
 from scipy.io import wavfile
 import soundfile as sf
 
+import torch
+
 
 model = whisper.load_model("base")
 selected_keys = {'ru', 'tr', 'ar'}
@@ -83,6 +85,36 @@ def main():
     # Calculate accuracy
     accuracy = calculate_accuracy(pre_processing_df)
     print("Accuracy: {:.2f}%".format(accuracy))
+    
+    
+def calc_score(embs1, embs2):
+    # Length Normalize
+    X = embs1 / torch.linalg.norm(embs1)
+    Y = embs2 / torch.linalg.norm(embs2)
+    # Score
+    similarity_score = torch.dot(X, Y) / ((torch.dot(X, X) * torch.dot(Y, Y)) ** 0.5)
+    similarity_score = (similarity_score + 1) / 2
+    return similarity_score
+
+    
+torch.no_grad()
+def verify_speakers(spk_model, anchor, group_file_list):
+    """
+    Verify if two audio files are from the same speaker or not.
+
+    Args:
+        path2audio_file1: path to audio wav file of speaker 1
+        path2audio_file2: path to audio wav file of speaker 2
+
+    Returns:
+        True if both audio files are from same speaker, False otherwise
+    """
+    anchor_embs = spk_model.get_embedding(anchor).squeeze()
+    scores = [
+        calc_score(anchor_embs, spk_model.get_embedding(cand).squeeze())
+        for cand in group_file_list
+    ]
+    return torch.stack(scores).argmax().item()
 
 
 if __name__ == "__main__":
